0b9358d2a437194bed494a7150e563b425af8ccb
Breno Dionisio <brnodionisio23@gmail.com>
2023-04-26 13:41:00 -0300

    header and footer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="nav">
-        <router-link to="/">Home</router-link> |
+        <router-link to="/">Home</router-link>
         <router-link to="/Pedidos">Pedidos</router-link>
     </div>
 </template>
@@ -10,5 +10,23 @@ export default {
 }
 </script>
 <style scoped>
+#nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
 
+#nav a {
+    color: #fcba03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+}
+
+#nav a:hover {
+    color: #fff ;
+}
 </style>
