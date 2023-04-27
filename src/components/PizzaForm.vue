cde04ef92b23850836dcca81bf406bf43a62277b
Breno Dionisio <brnodionisio23@gmail.com>
2023-04-27 14:47:45 -0300

    fake database connection

--- src/components/PizzaForm.vue
+++ src/components/PizzaForm.vue
@@ -13,6 +13,7 @@
                 <label for="mass">Escolha a massa:</label>
                 <select name="mass" id="mass" v-model="mass">
                     <option value="">Selecione a massa</option>
+                    <option v-for="mass in massas" :key="mass.id" value="mass.tipo">{{mass.tipo}}</option>
                 </select>
             </div>
 
@@ -20,23 +21,17 @@
                 <label for="filling">Selecione o seu recheio:</label>
                 <select name="filling" id="filling" v-model="filling">
                     <option value="">Selecione o tipo de recheio</option>
+                    <option v-for="filling in recheios" :key="filling.id" value="filling.tipo">{{filling.tipo}}</option>
                 </select>
             </div>
 
             <div id="optional-container" class="input-container">
                 <label id="optional-title"  for="optional">Selecione os opcionais:</label>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="optional" v-model="optional" value="salame">
-                    <span>salame</span>
+                <div class="checkbox-container" v-for="optional in opcionaisdata" :key="optional.id">
+                    <input type="checkbox" name="optional" v-model="opcionais" :value="optional.tipo">
+                    <span>{{optional.tipo}}</span>
                 </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="optional" v-model="optional" value="salame">
-                    <span>salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="optional" v-model="optional" value="salame">
-                    <span>salame</span>
-                </div>
+                
             </div>
 
             <div class="input-container">
@@ -48,7 +43,34 @@
 </template>
 <script>
 export default {
-    name: "PizzaForm"
+    name: "PizzaForm",
+    data() {
+        return {
+            massas: null,
+            recheios: null,
+            opcionaisdata: null,
+            nome: null,
+            massa: null,
+            recheio: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: "null"
+
+        }
+    },
+    methods: {
+        async getIngredients() {
+            const req = await fetch("http://localhost:3000/ingredientes");
+            const data = await req.json();
+
+            this.massas = data.massas;
+            this.recheios = data.recheios;
+            this.opcionaisdata = data.opcionais;
+        }
+    },
+    mounted() {
+        this.getIngredients()
+    },
 }
 </script>
 <style scoped>
